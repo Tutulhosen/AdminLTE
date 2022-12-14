0dcb4e4fbd9c20e7b90e5e091832e992336b8efe
tutul <tutulhosen2022@gmail.com>
2022-12-14 14:48:40 +0600

    laravel install and template setup

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * show the dashboard page
+     */
+    public function index()
+    {
+        return view('admin.dashboard');
+    }
+
+
+
+
+
+
+
+
+}
